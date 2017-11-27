b02a98c4c2bcc987319d58bd35f464c29ce5a62b
chiol <jcy6677@gmail.com>
2017-11-27 12:20:52 +0900

    학회메뉴추가

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [{
   }, {
     path: 'convenience',
     loadChildren: './convenience/convenience.module#ConvModule',
-  },{
+  }, {
     path: 'academy',
     loadChildren: './academy/academy.module#AcadModule',
   },
