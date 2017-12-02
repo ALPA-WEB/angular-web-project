adf5b130ed2dc0254e403135f8a9b49e362ab8e2
ParkBeomMin <club20608@gmail.com>
2017-12-03 02:50:52 +0900

    google auth store

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -1,41 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 
-import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import {AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
-import { NbMenuService, NbSidebarService } from '@nebular/theme';
-import { UserService } from '../../../@core/data/users.service';
-import { AnalyticsService } from '../../../@core/utils/analytics.service';
+import {NbMenuService, NbSidebarService} from '@nebular/theme';
+import {UserService} from '../../../@core/data/users.service';
+import {AnalyticsService} from '../../../@core/utils/analytics.service';
 
 @Component({
-  selector: 'ngx-header',
-  styleUrls: ['./header.component.scss'],
-  templateUrl: './header.component.html',
-  providers: [AngularFireAuth]
+    selector: 'ngx-header',
+    styleUrls: ['./header.component.scss'],
+    templateUrl: './header.component.html',
+    providers: [AngularFireAuth],
 })
 
 export class HeaderComponent implements OnInit {
 
 
-  @Input() position = 'normal';
+    @Input() position = 'normal';
 
-  user: any;
+    user: any;
 
-  userMenu = [{ title: 'Profile' },
-    { title: 'Log out' },
-    {
-      title: 'Log in', link: '/pages/ui-features/buttons',
-    },
+    userMenu = [{title: 'Profile'},
+        {title: 'Log out'},
+        {
+            title: 'Log in', link: '/pages/ui-features/buttons',
+        },
     ];
 
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
-              private userService: UserService,
+              public userService: UserService,
               private analyticsService: AnalyticsService,
               public afAuth: AngularFireAuth) {
   }
+  /*
   login() {
     // this.userService.googleLogin();
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
@@ -43,10 +44,15 @@ export class HeaderComponent implements OnInit {
   logout() {
     this.afAuth.auth.signOut();
   }
+  */
   ngOnInit() {
-    // this.user = {name : this.afAuth.auth.currentUser};
-    this.userService.getUsers()
-      .subscribe((users: any) => this.user = users.null);
+    // if(this.afAuth.authState) {
+    // this.afAuth.authState.subscribe((user: firebase.User) => this.user = {name : user.displayName});
+    // }else{
+      //this.user = {name : "Google Sign In!!"};
+    // this.userService.getUsers()
+    //   .subscribe((users: any) => this.user = users.nologin);
+    // }
   }
 
   toggleSidebar(): boolean {
