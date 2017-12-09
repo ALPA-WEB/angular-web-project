b92dbfd928f8b09da062c8ef5f3257adf8b19829
kangdongwon <dongwon5520@gmail.com>
2017-12-09 23:57:33 +0900

    ...

--- src/app/pages/dashboard/contacts/contacts.component.ts
+++ src/app/pages/dashboard/contacts/contacts.component.ts
@@ -28,32 +28,6 @@ export class ContactsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-<<<<<<< HEAD
-/*
-    this.userService.getUsers()
-      .subscribe((users: any) => {
-        this.contacts = [
-          {user: users.nick, type: 'mobile'},
-          {user: users.eva, type: 'home'},
-          {user: users.jack, type: 'mobile'},
-          {user: users.lee, type: 'mobile'},
-          {user: users.alan, type: 'home'},
-          {user: users.kate, type: 'work'},
-        ];
-
-        this.recent = [
-          {user: users.alan, type: 'home', time: '9:12 pm'},
-          {user: users.eva, type: 'home', time: '7:45 pm'},
-          {user: users.nick, type: 'mobile', time: '5:29 pm'},
-          {user: users.lee, type: 'mobile', time: '11:24 am'},
-          {user: users.jack, type: 'mobile', time: '10:45 am'},
-          {user: users.kate, type: 'work', time: '9:42 am'},
-          {user: users.kate, type: 'work', time: '9:31 am'},
-          {user: users.jack, type: 'mobile', time: '8:01 am'},
-        ];
-      });*/
-=======
->>>>>>> 5a20f044a43c4e375522b343e7845ff6d7ea352c
   }
 
   ngOnDestroy() {
