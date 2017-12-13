fe8c9e6bf70ffaf3036d2041286e212b2cb7e1ce
ParkBeomMin <club20608@gmail.com>
2017-12-13 09:46:22 +0900

    university info DB

--- src/app/pages/concil/university/university.component.ts
+++ src/app/pages/concil/university/university.component.ts
@@ -2,12 +2,30 @@
 /**
  * Created by chou6 on 2017-11-20.
  */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+interface Intro {
+    content: string;
+}
 @Component({
     selector: 'ngx-university',
     templateUrl: 'university.component.html',
     styleUrls: ['./university.component.scss'],
 })
-export class UniversityComponent {
+export class UniversityComponent implements OnInit{
+    introsCol: AngularFirestoreCollection<Intro>;
+    intros: Observable<Intro[]>;
+    intro: Intro = {
+      content: "",
+    }
 
+    constructor( private afs:AngularFirestore,) { }
+    
+    ngOnInit(){
+        this.introsCol = this.afs.collection('studentCouncil').doc('university').collection('intro');
+        this.intros = this.introsCol.valueChanges();    
+    }
 }
