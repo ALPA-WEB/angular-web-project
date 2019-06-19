cc4a07e905c721613528e88ad1bca3d75a3d5408
Choi Seungeon <chou610@naver.com>
2019-06-19 23:13:32 +0900

    [Update] replaced angularfire with vanilla firebase

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -1,80 +1,49 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-
-
 import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { allResolved } from 'q';
-
-let counter = 0;
-interface User {
-  uid: string;
-  email: string;
-  displayName: string;
-  ALPA: boolean;
-  HYCUBE: boolean;
-  JARAM: boolean;
-  ZERONE: boolean;
-  FIFO: boolean;
-}
+import 'firebase/firestore';
+import 'firebase/auth';
+import {User} from './user';
+const counter = 0;
 
 @Injectable()
-export class UserService {    
-  usercom: User = {uid: "", email:"", displayName:"", ALPA:false, HYCUBE: false,
-  JARAM: false,
-  ZERONE: false,
-  FIFO: false,};
-  user: Observable<User>;
-  constructor(private afAuth: AngularFireAuth,
-              private afs: AngularFirestore,
-              private router: Router) {
-      //// Get auth data, then get firestore user document || null
-      this.user = this.afAuth.authState
-        .switchMap(user => {
-          if (user) {
-            return this.afs.doc<User>(`users/${user.email}`).valueChanges();
-          } else {
-            return Observable.of(null);
-          }
-        });
-  }
-  googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    return this.oAuthLogin(provider);
-  }
-  private oAuthLogin(provider) {
-    return this.afAuth.auth.signInWithPopup(provider)
-      .then((credential) => {
-        this.updateUserData(credential.user);
-      });
-  }
-  private updateUserData(user) {
-    // Sets user data to firestore on login
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.email}`);
-    userRef.valueChanges().subscribe((data) => this.usercom = data);
-    const data: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      ALPA: false,
-      HYCUBE: false,
-      JARAM: false,
-      ZERONE: false,
-      FIFO: false,
-    }
-      // alert(user.email + "|" + this.usercom.email);
-    if(user.email == this.usercom.email){
-      return null;
-    }else{
-      return userRef.set(data);
-    }
-  }
-  signOut() {
-    this.afAuth.auth.signOut().then(() => {
-        this.router.navigate(['/']);
+export class UserService {
+  private firebaseApp: firebase.app.App;
+  private db: firebase.firestore.Firestore;
+  private auth: firebase.auth.Auth;
+  private googleProvider: firebase.auth.GoogleAuthProvider;
+  private currentUser: User;
+  constructor(private router: Router) {
+    this.firebaseApp = firebase.initializeApp({
+      authDomain: 'forestvue-8424e.firebaseapp.com',
+      apiKey: 'AIzaSyC_bQgvZYtcQjwmmXR2I1YfyZLM7P1t9tQ',
+      projectId: 'forestvue-8424e',
     });
+    this.db = this.firebaseApp.firestore();
+    this.auth = this.firebaseApp.auth();
+    this.googleProvider = new firebase.auth.GoogleAuthProvider();
+    this.auth.onAuthStateChanged(this.updateUser);
+    this.currentUser = new User();
+  }
+  public login(data): void {
+    this.auth.signInWithEmailAndPassword(data.id, data.pw);
+  }
+  public googleLogin(): void {
+    this.auth.signInWithPopup(this.googleProvider);
+  }
+  private updateUser(user): void {
+    if (!user || user.uid === undefined) {return; }
+    this.findUser(user.uid).then(res => {
+        if (res.exists) {
+        }else {
+        }
+    });
+  }
+  findUser (uid): Promise<firebase.firestore.DocumentSnapshot> {
+    return this.db.collection('').doc(uid)
+      .get();
+  }
+  signOut(): void {
+    this.auth.signOut();
   }
 }
